a9cf1d6b1a41550ea122a5ec4bd2f23988e69a5e
Angel <angel.lladonosa.panal@iesciudadjardin.com>
2022-04-22 12:16:51 +0200

    link routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,29 @@
-import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
+import {NgModule} from '@angular/core';
+import {RouterModule, Routes} from '@angular/router';
+import {AboutComponent} from "./pages/about/about.component";
+import {SkillsComponent} from "./pages/skills/skills.component";
+import {EducationComponent} from "./pages/education/education.component";
+import {ContactComponent} from "./pages/contact/contact.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'about',
+    component: AboutComponent
+  },{
+    path: 'skills',
+    component: SkillsComponent
+  },{
+    path: 'education',
+    component: EducationComponent
+  },{
+    path: 'contact',
+    component: ContactComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {
+}
