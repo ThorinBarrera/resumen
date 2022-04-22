40a9e5c60c80b591cda0f9c14ce386696539c761
Angel <angel.lladonosa.panal@iesciudadjardin.com>
2022-04-22 11:59:48 +0200

    toggle show class

--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -6,8 +6,11 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./navbar.component.scss']
 })
 export class NavbarComponent implements OnInit {
+  show: boolean;
 
-  constructor() { }
+  constructor() {
+    this.show = false;
+  }
 
   ngOnInit(): void {
   }
